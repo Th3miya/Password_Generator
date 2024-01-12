eec244f3f4e2c0b43101369bd9d785bbe617b20b
Themiya <themiya0git@gmail.com>
2024-01-12 18:16:40 +0530

    Generating Password Process Done

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import string
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_password(min_length, numbers=True, special=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_char = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special:
+        characters += special_char
+
+    password = ""
+    meets_req = False
+    has_numbers = False
+    has_special = False
+
+    while not meets_req or len(password) < min_length:
+        new_char = random.choice(characters)
+        password += new_char
+
+        if new_char in digits:
+            has_numbers = True
+        if new_char in special_char:
+            has_special = True
+
+        meets_req = True
+        if numbers:
+            meets_req = has_numbers
+        if special:
+            meets_req = meets_req and has_special
+
+    print(password)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+generate_password(10)
